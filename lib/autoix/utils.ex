9f319c089b6e5dc46ee6b55115afe030ee3296bb
tyde81 <pedrohfonseca.1981@gmail.com>
2023-01-15 20:58:07 -0300

    feat: change guards in utils module

--- lib/autoix/utils.ex
+++ lib/autoix/utils.ex
@@ -1,23 +1,36 @@
 defmodule Autoix.Utils do
   alias Autoix.Flow
 
-  def sleep(ms) do
+  def sleep(ms) when is_number(ms) do
     Process.sleep(ms)
   end
 
-  defmacro goto(fn_name, module) do
+  def sleep(_) do
+    {:error, "invalid type"}
+  end
+
+  defmacro goto(fn_name, module) when is_binary(fn_name) and not is_nil(module) do
     fun = String.to_atom(fn_name)
 
     quote do
-      Flow.apply_function(unquote(module), {unquote(fun), unquote(fn_name), []})
+      case Keyword.has_key?(unquote(module).__info__(:functions), unquote(fun)) do
+        true -> Flow.apply_function(unquote(module), {unquote(fun), unquote(fn_name), []})
+        false -> raise("invalid task")
+      end
     end
   end
 
-  defmacro goto(fn_name) do
+  defmacro goto(fn_name) when is_binary(fn_name) do
     fun = String.to_atom(fn_name)
 
     quote do
-      Flow.apply_function(__ENV__.module, {unquote(fun), unquote(fn_name), []})
+      case Keyword.has_key?(__ENV__.module.__info__(:functions), unquote(fun)) do
+        true ->
+          Flow.apply_function(__ENV__.module, {unquote(fun), unquote(fn_name), []})
+
+        false ->
+          raise("invalid task")
+      end
     end
   end
 end
