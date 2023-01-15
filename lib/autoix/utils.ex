78096be159841b6e5847833d480f696ebc93faca
tyde81 <pedrohfonseca.1981@gmail.com>
2023-01-15 20:23:45 -0300

    feat: goto function

--- lib/autoix/utils.ex
+++ lib/autoix/utils.ex
@@ -1,5 +1,23 @@
 defmodule Autoix.Utils do
+  alias Autoix.Flow
+
   def sleep(ms) do
     Process.sleep(ms)
+  end
+
+  defmacro goto(fn_name, module) do
+    fun = String.to_atom(fn_name)
+
+    quote do
+      Flow.apply_function(unquote(module), {unquote(fun), unquote(fn_name), []})
+    end
+  end
+
+  defmacro goto(fn_name) do
+    fun = String.to_atom(fn_name)
+
+    quote do
+      Flow.apply_function(__ENV__.module, {unquote(fun), unquote(fn_name), []})
+    end
   end
 end
